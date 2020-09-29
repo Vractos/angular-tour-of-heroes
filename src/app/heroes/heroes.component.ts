96fd7a881791c1d7b6760af3f61476dd84e83d6d
Vractos <53495782+Vractos@users.noreply.github.com>
2020-09-29 13:49:47 -0300

    [INIT] Project

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { IHero } from '../hero'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero: IHero = {
+    id: 1,
+    name: 'Windstorm'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
